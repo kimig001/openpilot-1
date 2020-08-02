d08ff58875fcdd9d2ef9f4493a271b57fcdbf8e6
azure93 <azure93@gmail.com>
2020-08-02 16:42:30 +0900

    Update interface.py
    add K7_Hybrid

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -147,7 +147,24 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 1640. + STD_CARGO_KG
       ret.wheelbase = 2.845
 
-
+    elif candidate == CAR.K7_HYBRID:
+      
+      tire_stiffness_factor = 0.385
+      ret.mass = 1685. + STD_CARGO_KG
+      ret.wheelbase = 2.885
+      ret.steerRatio = 13.5
+      ret.lateralTuning.init('lqr')
+      ret.lateralTuning.lqr.scale = 2000.0
+      ret.lateralTuning.lqr.ki = 0.01
+      ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
+      ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
+      ret.lateralTuning.lqr.c = [1., 0.]
+      ret.lateralTuning.lqr.k = [-100., 450.]
+      ret.lateralTuning.lqr.l = [0.22, 0.318]
+      ret.lateralTuning.lqr.dcGain = 0.003
+      ret.steerActuatorDelay = 0.1
+      ret.steerRateCost = 0.5
+      ret.steerLimitTimer = 0.8    
 
     # LQR control by default for lateral control.
 
